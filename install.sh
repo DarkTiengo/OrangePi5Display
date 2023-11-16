fb1f4b2bb1f24eccd8de29221be0beb155ae267d
DarkTiengo <r_tiengo@hotmail.com>
2023-11-15 23:10:06 -0300

    add Install Script

---
+++ install.sh
@@ -0,0 +1,39 @@
+sudo apt update && sudo apt install git swig python3-dev python3-setuptools python3-pip libgpiod-dev -y
+
+git clone --recursive https://github.com/orangepi-xunlong/wiringOP-Python -b next
+cd wiringOP-Python
+python3 generate-bindings.py > bindings.i
+sudo python3 setup.py install
+
+
+arquivo="/etc/udev/rules.d/99-rk-device-permissions.rules"
+echo 'SUBSYSTEM=="i2c-dev", GROUP="i2c", MODE="0666"' | sudo tee -a "$arquivo" > /dev/null
+
+cd /tmp
+git clone https://github.com/DarkTiengo/OrangePi5Display
+cd OrangePi5Display
+python3-pip install -r requeriments.txt
+sudo cp OrangeDisplay.py /usr/bin
+cd ..
+sudo rm -r OrangePi5Display
+
+cat <<EOF | sudo tee /etc/systemd/system/OrangePi5Display.service
+[Unit]
+Description=Oled Display System Information
+After=multi-user.target
+
+[Service]
+Type=idle
+ExecStart=/usr/bin/python3 /usr/bin/OrangeDisplay.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable OrangePi5Display.service
+sudo systemctl start OrangePi5Display.service
+
+cd -
+
+echo "Installation Complete."
