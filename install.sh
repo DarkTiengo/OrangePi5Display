2c9f7e00fbf7ba764f26ecfb2ef6980b4f180184
Rafael Tiengo <43833939+DarkTiengo@users.noreply.github.com>
2023-12-07 17:27:13 -0300

    Update install.sh for ubuntu

--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@ sudo apt update && sudo apt install git swig python3-dev python3-setuptools pyth
 git clone --recursive https://github.com/orangepi-xunlong/wiringOP-Python -b next
 cd wiringOP-Python
 python3 generate-bindings.py > bindings.i
-sudo python3 setup.py install
+python3 setup.py install
 
-
-arquivo="/etc/udev/rules.d/99-rk-device-permissions.rules"
-echo 'SUBSYSTEM=="i2c-dev", GROUP="i2c", MODE="0666"' | sudo tee -a "$arquivo" > /dev/null
+usermod $USER -aG i2c
 
 cd /tmp
 git clone https://github.com/DarkTiengo/OrangePi5Display.git
 cd OrangePi5Display
 pip install -r requeriments.txt
-sudo cp OrangeDisplay.py /usr/bin
-sudo cp display_datas.conf /usr/local/etc
+cp OrangeDisplay.py /usr/bin
+cp display_datas.conf /usr/local/etc
 cd ..
-sudo rm -r OrangePi5Display
+rm -r OrangePi5Display
 
 cat <<EOF | sudo tee /etc/systemd/system/OrangePi5Display.service
 [Unit]
@@ -31,9 +29,9 @@ ExecStart=/usr/bin/python3 /usr/bin/OrangeDisplay.py
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload
-sudo systemctl enable OrangePi5Display.service
-sudo systemctl start OrangePi5Display.service
+systemctl daemon-reload
+systemctl enable OrangePi5Display.service
+systemctl start OrangePi5Display.service
 
 cd -
 
